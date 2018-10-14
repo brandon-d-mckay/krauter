a268f92d276649c18687f0f00e398987f8cf875f
Brandon D. McKay <brandon.d.mckay@gmail.com>
2018-10-14 19:00:05 -0400

    Add eslint configuration

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const methods = require("methods").concat("all");
+const express = require('express');
+const methods = require('methods').concat('all');
 const privates = Symbol();
 
 class Kraut {
@@ -15,7 +15,8 @@ methods.forEach(method =>
 	Kraut.prototype[method] = function(... args) {
 		this[privates].krauter[method](this[privates].path, ... args);
 		return this;
-});
+	}
+);
 
 class Krauter {
 	constructor(execute, options) {
@@ -45,10 +46,10 @@ methods.forEach(method => Krauter.prototype[method] = function(path, ... args) {
 			return (req, res, next) => {
 				delete req.data;
 				next();
-			}
+			};
 		}
 		
-		else if(typeof arg === "string") {
+		else if(typeof arg === 'string') {
 			return (req, res, next) => {
 				this[privates].execute(... parse(arg, req)).then(result => {
 					req.data = result;
@@ -57,7 +58,7 @@ methods.forEach(method => Krauter.prototype[method] = function(path, ... args) {
 			};
 		}
 		
-		else if(typeof arg === "object") {
+		else if(typeof arg === 'object') {
 			return (req, res, next) => {
 				const results = {};
 				
@@ -65,14 +66,14 @@ methods.forEach(method => Krauter.prototype[method] = function(path, ... args) {
 					this[privates].execute(... parse(arg[key], req)).then(result => {
 						results[key] = result;
 					}))
-				).then(ignored => {
+				).then(() => {
 					req.data = results;
 					next();
 				}).catch(next);
 			};
 		}
 		
-		else if(typeof arg === "function" && arg.length <= 1) {
+		else if(typeof arg === 'function' && arg.length <= 1) {
 			return (req, res, next) => {
 				const data = req.data;
 				req.next = req.data = undefined;
@@ -85,14 +86,14 @@ methods.forEach(method => Krauter.prototype[method] = function(path, ... args) {
 				}
 				
 				next();
-			}
+			};
 		}
 
-		else if(typeof arg === "number") {
+		else if(typeof arg === 'number') {
 			return (req, res, next) => {
 				res.status(arg);
 				next();
-			}
+			};
 		}
 
 		else return arg;
@@ -101,15 +102,16 @@ methods.forEach(method => Krauter.prototype[method] = function(path, ... args) {
 	return this;
 });
 
-["use", "param"].forEach(method =>
+['use', 'param'].forEach(method =>
 	Krauter.prototype[method] = function(... args) {
 		this[privates].router[method](... args);
-});
+	}
+);
 
 function parse(string, req, values = [], types = []) {
 	return [
 		string.replace(/(^|[^:]):(?:{(\w+)(?:\(([\d, ]*)\))?})?([\w]+(?:\.[\w]+)*):/g, (match, precedingChar, typeProperty, typeArguments, reference) =>
-			precedingChar + "?param" + (0&types.push({property: typeProperty, arguments: typeArguments}) || values.push(reference.split(".").reduce((o, p) => o[p], req))) + "?"
+			precedingChar + '?param' + (0&types.push({property: typeProperty, arguments: typeArguments}) || values.push(reference.split('.').reduce((o, p) => o[p], req))) + '?'
 		),
 		values,
 		types
@@ -120,12 +122,12 @@ const krauter = (... args) => new Krauter(... args);
 
 Object.assign(krauter, {
 	pg: Object.assign((conn, options) => krauter(krauter.pg.executor(conn), options), {
-		executor: conn => (query, values) => conn.query(query.replace(/\?param(\d+)\?/g, "$$$1"), values)
+		executor: conn => (query, values) => conn.query(query.replace(/\?param(\d+)\?/g, '$$$1'), values)
 	}),
 	
 	mysql: Object.assign((conn, options) => krauter(krauter.mysql.executor(conn), options), {
 		executor: conn => (query, values) => new Promise((resolve, reject) =>
-			conn.query(query.replace(/\?param\d+\?/g, "?"), values, (err, results, fields) =>
+			conn.query(query.replace(/\?param\d+\?/g, '?'), values, (err, results, fields) =>
 				err ? reject(err) : resolve(Object.assign(results, {fields}))
 			)
 		)
@@ -133,24 +135,24 @@ Object.assign(krauter, {
 	
 	mssql: Object.assign((conn, options) => krauter(krauter.mssql.executor(conn), options), {
 		executor: conn => {
-			const mssql = require("mssql");
+			const mssql = require('mssql');
 			
 			function getMssqlType(type) {
 				const property = mssql[type.property];
-				return type.arguments ? property(... type.arguments.split(",").map(arg => +arg.trim())) : property;
+				return type.arguments ? property(... type.arguments.split(',').map(arg => +arg.trim())) : property;
 			}
 			
 			return (query, values, types) => {
 				const request = conn.request();
 				for(let i = 0; i < values.length; i++) request.input(`param${i + 1}`, ... (types[i].property ? [getMssqlType(types[i]), values[i]] : [values[i]]));
-				return request.query(query.replace(/\?(param\d+)\?/g, "@$1"));
+				return request.query(query.replace(/\?(param\d+)\?/g, '@$1'));
 			};
 		}
 	}),
 	
 	sqlite3: Object.assign((conn, options) => krauter(krauter.sqlite3.executor(conn), options), {
 		executor: conn => (query, values) => new Promise((resolve, reject) =>
-			conn.query(query.replace(/\?param\d+\?/g, "?"), values, (err, rows) =>
+			conn.query(query.replace(/\?param\d+\?/g, '?'), values, (err, rows) =>
 				err ? reject(err) : resolve(rows)
 			)
 		)
